703f97df04f40773985d03346852d44d632b8dd7
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-04-28 14:51:39 +0200

    Add all config files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # webpack-tailwind-config
+
+npm init -y
+
+npm install --save-dev webpack webpack-cli webpack-dev-server style-loader css-loader postcss postcss-loader
+
+npm install -D tailwindcss@latest autoprefixer@latest
+
+mkdir dist src && touch dist/index.html src/index.js src/style.css
+
+webpack.config.js
+
+npx tailwindcss init
+
+postcss.config.js
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    watchContentBase: true,
+  },
+};
